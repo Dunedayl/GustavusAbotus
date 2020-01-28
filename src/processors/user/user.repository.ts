fc9ef6e24990edbb1c24f90ab96a4b64e1e9fafd
Gorschynskyi <gorschynskyi@gmail.com>
2020-01-29 00:51:22 +0200

    Removed reactions.

--- src/processors/user/user.repository.ts
+++ src/processors/user/user.repository.ts
@@ -1,6 +1,6 @@
 import { User } from "./user.model";
 
 export const userRepository: User[] = [
-    { id: '371670423999610880',  reactions: ['0️⃣', '🚬'] },
-    { id: '281847177213509643', reactions: ['🦇'] },
+    { id: '371670423999610880',  reactions: [] },
+    { id: '281847177213509643', reactions: [] },
 ];
